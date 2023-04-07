8ca2617f7981eb1952961c2527a7e73d4d341e52
CiranoB <ciranobelardony@gmail.com>
2023-04-06 22:09:58 -0300

    Initializing Spring Boot Sample

---
+++ backend/src/main/java/cirano/hrops/HropsApplication.java
@@ -0,0 +1,13 @@
+package cirano.hrops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HropsApplication.class, args);
+	}
+
+}
